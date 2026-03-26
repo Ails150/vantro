57d24d4564cdc94c893b4984b6eae3a4c2c20f59
Ails150 <ailsdoherty00@gmail.com>
2026-03-26 16:10:13 +0000

    add PWA icons

---
+++ fix-icons2.js
@@ -0,0 +1,12 @@
+﻿const fs = require("fs")
+
+// Create minimal valid PNG files (1x1 teal pixel, browsers will scale)
+// These are base64 encoded minimal PNGs
+const png192 = Buffer.from(
+  "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==",
+  "base64"
+)
+
+fs.writeFileSync("public/icon-192.png", png192)
+fs.writeFileSync("public/icon-512.png", png192)
+console.log("PNG icons created")
